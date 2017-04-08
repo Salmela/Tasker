90228a1b61a258ae4e5a6e95cc6bf57e11c6b3d0
Aleksi Salmela <aleksi.salmela@cs.helsinki.fi>
2017-04-08 11:26:24 +0300

    Add skeleton classes for the project.

---
+++ backend.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class TaskState
+{
+public:
+	TaskState(std::string name);
+	void rename(std::string name);
+private:
+	std::string name;
+};
+
+class Task
+{
+public:
+	void setName(std::string name);
+	std::string getName() const;
+	void setState(TaskState *state);
+	TaskState *getState() const;
+	void setDescription(std::string text);
+	std::string getDescription() const;
+private:
+	std::string name;
+	std::string desc;
+	TaskState *state;
+};
+
+class TaskList
+{
+public:
+	void addTask(Task *task);
+	void removeTask(Task *task);
+private:
+	std::vector<Task> tasks;
+};
+
+
+class TaskListView
+{
+public:
+	void render();
+};
+
+class TaskView
+{
+public:
+	void render();
+};
